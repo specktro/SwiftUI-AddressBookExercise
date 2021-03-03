588f461029236abbaa4243f9761eac4ead2d402b
Ben Stone <benjamin.h.stone@gmail.com>
2021-03-03 16:21:25 -0500

    add starter code

--- AddressBookWithPropertyWrappers/AddressBookWithPropertyWrappersApp.swift
+++ AddressBookWithPropertyWrappers/AddressBookWithPropertyWrappersApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AddressBookWithPropertyWrappersApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AddressBookView()
         }
     }
 }
